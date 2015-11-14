997dbd5424abcbba8f5f46e95dd9418232ae5f35
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-11-14 14:55:07 +0100

    refactoring

--- PowerManager/usr/lib/power-manager/DBusService/PowerManagerService.py
+++ PowerManager/usr/lib/power-manager/DBusService/PowerManagerService.py
@@ -13,7 +13,6 @@ class PowerManagerService(dbus.service.Object):
     Start a Powermanager DBus service, with some custom methods
     to switch a current power mode for computer
     """
-
     def __init__(self, name, power_manager, logger):
 
         bus = dbus.SystemBus()
@@ -24,39 +23,39 @@ class PowerManagerService(dbus.service.Object):
 
         dbus.service.Object.__init__(self, dbus.service.BusName(name, bus), "/" + name.replace('.', '/'))
 
+
     """
     DBus method to optimize power usage
     with respect to current conditions
     """
-
     @dbus.service.method('org.sensey.PowerManager')
     def optimize(self, options=None):
         if self.__upower.is_battery():
             return self.powersave(options)
         return self.perfomance(options)
 
+
     """
     DBus method to run powersave mode
     """
-
     @dbus.service.method('org.sensey.PowerManager')
     def powersave(self, options):
         self.__power_manager.powersave()
         self.status_changed()
 
+
     """
     DBus method to run perfomance mode
     """
-
     @dbus.service.method('org.sensey.PowerManager')
     def perfomance(self, options):
         self.__power_manager.perfomance()
         self.status_changed()
 
+
     """
     DBus method to get current status of all modules
     """
-
     @dbus.service.method('org.sensey.PowerManager')
     def status(self, options):
         response = {}
@@ -64,11 +63,11 @@ class PowerManagerService(dbus.service.Object):
             response[switcher.__str__()] = switcher.is_powersave
         return json.dumps(response)
 
+
     """
     DBus signal to notify other
     object about changed status
     """
-
     @dbus.service.signal(dbus_interface='org.sensey.PowerManager', signature='')
     def status_changed(self):
         pass
--- PowerManager/usr/lib/power-manager/Powermanager/Powermanager.py
+++ PowerManager/usr/lib/power-manager/Powermanager/Powermanager.py
@@ -9,7 +9,6 @@ class Powermanager():
     Initialize a main power manager object
     set up all power switchers
     """
-
     def __init__(self, logger):
         self._switchers = []
         for (name, module) in inspect.getmembers(Switchers, inspect.ismodule):
@@ -17,46 +16,47 @@ class Powermanager():
             self._switchers.append(identifier())
         self._logger = logger
 
+
     """
     try to switch a computer to powersave mode
     use all switchers to get a string command
     than run each command in custom thread
     """
-
     def powersave(self):
         self._log('powersave')
         for switcher in self._switchers:
             for command in switcher.powersave():
                 self._run(command)
 
+
     """
     try to switch a computer to perfomance mode
     use all switchers to get a string command
     than run each command in custom thread
     """
-
     def perfomance(self):
         self._log('perfomance')
         for switcher in self._switchers:
             for command in switcher.perfomance():
                 self._run(command)
 
+
     @property
     def switchers(self):
         return self._switchers
 
+
     """
     write logs using given logger class
     """
-
     def _log(self, text):
         if self._logger is not None:
             self._logger.debug(text)
 
+
     """
     try to run a command in a custom thread
     """
-
     def _run(self, command):
         self._logger.info(command)
         (Command(command)).run()
